3b7492f0e8bb3ebb14c97ca89811d461cdda9e66
Adam Davidson <adam.davo@gmail.com>
2024-06-05 10:19:34 +1000

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)"],
+        mono: ["var(--font-roboto-mono)"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
