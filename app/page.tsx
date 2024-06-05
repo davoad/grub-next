d90a8f15ac85ee76003316c075900a68efdeaf38
Adam Davidson <adam.davo@gmail.com>
2024-06-05 17:11:25 +1000

    Added tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
+    <div className="flex h-screen w-full items-center justify-center">
       <h1 className="text-4xl font-medium">Good Grub</h1>
     </div>
   );
