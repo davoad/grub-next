5189d2b87128cd94f57181ae517b2bae8ebe119f
Adam Davidson <adam.davo@gmail.com>
2024-06-12 17:42:38 +1000

    Change header colour to violet

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ import { UserButton, ClerkLoading, ClerkLoaded } from "@clerk/nextjs";
 
 export const Header = () => {
   return (
-    <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14">
+    <header className="bg-gradient-to-b from-violet-700 to-violet-500 px-4 py-8 lg:px-14">
       <div className="mx-auto max-w-screen-2xl">
         <div className="flex w-full items-center justify-between">
           <div className="flex items-center lg:gap-x-16">
